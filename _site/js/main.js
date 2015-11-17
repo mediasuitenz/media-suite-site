11d5c786b9e4bf9ee63b7d408e6fe352b6fa21e9
Julia <julia@mediasuite.co.nz>
2015-11-18 12:02:22 +1300

    updated fontello icons, unfixed menu position on mobile

--- _site/js/main.js
+++ _site/js/main.js
@@ -40,8 +40,10 @@ function init () {
           else menu.addClass('on');
         });
       }
+      // If home page, add relevant styles for one page scroll plugin:
       if(_isHomePage()){
         $('body, html').addClass('one-pager');
+      // Otherwise, destroy plugin and remove specific styles:
       } else {
         $('#page-header').addClass('on');
         $('body, html').removeClass('one-pager');
